e3933f73bd45d5b268adda813bea05c8b130511e
MugaruraEdwin <mugarura13@gmail.com>
2024-08-26 18:07:59 +0300

    Added front-end 01

---
+++ index.js
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+
+const app = express();
+const port = 3000;
+
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static("public"))
+
+app.get('/', (req,res) => {
+    res.render('index.ejs')
+})
+
+
+app.listen(port, () => {
+    console.log(`Listening on port: ${port}`)
+});
